c3a938320dc4a8b0141f7d439652c78e3fbb8611
Abhinav Singh <abhinav.singh07@sap.com>
2024-01-30 12:01:17 +0000

    added tests

--- srv/src/main/java/com/sap/cap/incident_management/handler/ProcessorServiceHandler.java
+++ srv/src/main/java/com/sap/cap/incident_management/handler/ProcessorServiceHandler.java
@@ -2,30 +2,39 @@ package com.sap.cap.incident_management.handler;
 
 import cds.gen.processorservice.Incidents;
 import cds.gen.processorservice.ProcessorService_;
+import cds.gen.sap.capire.incidents.*;
+
+import com.sap.cds.ql.Select;
 import com.sap.cds.services.ErrorStatuses;
 import com.sap.cds.services.ServiceException;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Locale;
+
 
 @Component
 @ServiceName(ProcessorService_.CDS_NAME)  
 public class ProcessorServiceHandler implements EventHandler {
 
+	@Autowired
+	private PersistenceService db;
 /*
  * Change the urgency of an incident to "high" if the title contains the word "urgent"
  */
 	private static final Logger logger = LoggerFactory.getLogger(ProcessorServiceHandler.class);
 	@Before(event = CqnService.EVENT_CREATE)  
 	public void ensureHighUrgencyForIncidentsWithUrgentInTitle(List<Incidents> incidents) {  
-		for (Incidents incident : incidents) { 
-            System.out.println(incident.getTitle());
+		for (Incidents incident : incidents) {
 			if (incident.getTitle().toLowerCase(Locale.ENGLISH).contains("urgent") &&
             incident.getUrgencyCode() == null || !incident.getUrgencyCode().equals("H")) {  
                 incident.setUrgencyCode("H");  
@@ -39,11 +48,11 @@ public class ProcessorServiceHandler implements EventHandler {
  */
     @Before(event = CqnService.EVENT_UPDATE)  
 	public void onUpdate(Incidents incident) { 
-        
-        if(incident.getStatusCode().equals("C")){
+        Incidents in = db.run(Select.from((Class<Incidents_>) Incidents_.class).where(i -> i.ID().eq(incident.getId()))).single(Incidents.class);
+		if(in.getStatusCode().equals("C")){
             throw new ServiceException(ErrorStatuses.CONFLICT, "Can't modify a closed incident");
         }
-          
+
 	}
 
 
--- srv/src/test/java/com/sap/cap/incident_management/IncidentsODataTests.java
+++ srv/src/test/java/com/sap/cap/incident_management/IncidentsODataTests.java
@@ -6,9 +6,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.containsString;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.hamcrest.Matchers.hasSize;
+import com.jayway.jsonpath.JsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,6 +19,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class IncidentsODataTests {
 	
 	private static final String incidentsURI = "/odata/v4/ProcessorService/Incidents";
+    private static final String customerURI = "/odata/v4/ProcessorService/Customers";
+    private static final String expandEntityURI = "/odata/v4/ProcessorService/Customers?$select=firstName&$expand=incidents";
 
     @Autowired
     private MockMvc mockMvc;
@@ -25,10 +28,86 @@ class IncidentsODataTests {
     @Test
     @WithMockUser(username = "alice")
     void incidentReturned() throws Exception {
-        mockMvc.perform(get(incidentsURI + "?$top=1"))
+        mockMvc.perform(get(incidentsURI))
             .andExpect(status().isOk())
-                .andExpect(jsonPath("$.value", hasSize(4)))
-            .andExpect(jsonPath("$.value[0].title").value(containsString("Solar panel broken")));
+                .andExpect(jsonPath("$.value", hasSize(4)));
+
+    }
+    @Test
+    @WithMockUser(username = "alice")
+    void customertReturned() throws Exception {
+        mockMvc.perform(get(customerURI))
+            .andExpect(status().isOk())
+                .andExpect(jsonPath("$.value", hasSize(3)));
+
+    }
+
+    @Test
+    @WithMockUser(username = "alice")
+    void expandEntityEndpoint() throws Exception {
+        mockMvc.perform(get(expandEntityURI))
+        .andExpect(jsonPath("$.value[0].incidents[0]").isMap())
+        .andExpect(jsonPath("$.value[0].incidents[0]").isNotEmpty());
+
+    }
+    @Test
+    @WithMockUser(username = "alice")
+    void createIncident() throws Exception {
+        String incidentJson = "{ \"title\": \"Urgent attention required!\", \"status_code\": \"N\", \"urgency_code\": \"M\", \"IsActiveEntity\": true }";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/odata/v4/ProcessorService/Incidents")
+                .content(incidentJson)
+                .contentType("application/json")
+                .accept("application/json"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").isMap())
+                .andExpect(jsonPath("$.title").value("Urgent attention required!"))
+                .andExpect(jsonPath("$.status_code").value("N"))
+                .andExpect(jsonPath("$.urgency_code").value("H"));
+    }
+
+    @Test
+    @WithMockUser(username = "alice")
+    void updateIncident() throws Exception {
+        String incidentCreateJson = "{ \"title\": \"Urgent attention required!\", \"status_code\": \"N\", \"IsActiveEntity\": true }";
+        String incidentUpdateJson = "{\"status_code\": \"C\"}";
+        String closedIncidentUpdateJson = "{\"status_code\": \"I\"}";
+
+        MvcResult createResult=  mockMvc.perform(MockMvcRequestBuilders.post("/odata/v4/ProcessorService/Incidents")
+                .content(incidentCreateJson)
+                .contentType("application/json")
+                .accept("application/json"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value("Urgent attention required!"))
+                .andExpect(jsonPath("$.status_code").value("N"))
+                .andReturn();
+
+                String createResponseContent = createResult.getResponse().getContentAsString();
+                String ID = JsonPath.read(createResponseContent, "$.ID");
+                System.out.println("Incident ID : " + ID);
+
+        MvcResult updateResult=  mockMvc.perform(MockMvcRequestBuilders.patch("/odata/v4/ProcessorService/Incidents(ID="+ID+",IsActiveEntity=true)")
+                .content(incidentUpdateJson)
+                .contentType("application/json")
+                .accept("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Urgent attention required!"))
+                .andExpect(jsonPath("$.status_code").value("C"))
+                .andReturn();
+
+                String updateResponseContent = updateResult.getResponse().getContentAsString();
+                String statusCode = JsonPath.read(updateResponseContent, "$.status_code");
+                System.out.println("status code : " + statusCode);
+
+
+            mockMvc.perform(MockMvcRequestBuilders.patch("/odata/v4/ProcessorService/Incidents(ID="+ID+",IsActiveEntity=true)")
+                .content(closedIncidentUpdateJson)
+                .contentType("application/json")
+                .accept("application/json"))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.error.message").value("Can't modify a closed incident"));
+
+
 
     }
 
